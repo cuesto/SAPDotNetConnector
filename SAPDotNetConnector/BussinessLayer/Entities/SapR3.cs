d66849d1c8b9545366d4c44875693dc968a23ce4
Jonathan Cuesto <34cuesto@gmail.com>
2014-09-04 11:36:48 -0400

    add an entity class for SAPR3

---
+++ SAPDotNetConnector/BussinessLayer/Entities/SapR3.cs
@@ -0,0 +1,64 @@
+﻿using SAPDotNetConnector.DataAccess;
+using SAP.Middleware.Connector;
+using System;
+using System.Collections.Generic;
+
+namespace SAPDotNetConnector
+{
+    class SapR3
+    {
+        public RfcDestination rfcDest;
+
+        public SapR3()
+        {
+            SAPSystemConnect sapCfg = new SAPSystemConnect();
+
+            RfcDestinationManager.RegisterDestinationConfiguration(sapCfg);
+
+            //System Id
+            rfcDest = RfcDestinationManager.GetDestination("DAM");
+        }
+
+        public IRfcFunction getFunction(RfcDestination destination, string function, Dictionary<string, string> param)
+        {
+            try
+            {
+                RfcRepository repo = destination.Repository;
+                //declare function
+                IRfcFunction listfunc = repo.CreateFunction(function);
+                //send parameters
+                foreach (var p in param)
+                {
+                    if (p.Key != "")
+                    {
+                        listfunc.SetValue(p.Key, p.Value);
+                    }
+                }
+                //call function
+                listfunc.Invoke(destination);
+
+                //get table
+                return listfunc;
+            }
+            catch (RfcCommunicationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (RfcLogonException e)
+            {
+                // user could not logon...
+                Console.WriteLine("Cannot Connect - " + e.Message);
+            }
+            catch (RfcAbapRuntimeException e)
+            {
+                // serious problem on ABAP system side...
+            }
+            catch (RfcAbapBaseException e)
+            {
+                // The function module returned an ABAP exception, an ABAP message
+                // or an ABAP class-based exception...
+            }
+            return null;
+        }
+    }
+}
--- SAPDotNetConnector/DataAccess/SAPSystemConnect.cs
+++ SAPDotNetConnector/DataAccess/SAPSystemConnect.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Interfaz_Ivend_SAP.DataAccess
+namespace SAPDotNetConnector.DataAccess
 {
     class SAPSystemConnect : IDestinationConfiguration
     {
